880bcab93704ac375ea888195dfacf7ff212352b
EvertonSouzaa <everton.silva.souza@outlook.com>
2021-10-14 22:41:58 -0300

    organização src

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {theme} from './src/global/styles/theme';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return (
     <View>
-      <Text>First Commit</Text>
+      <Icon name="mail" size={30} />
+      <Text
+        style={{
+          fontSize: 50,
+          color: theme.colors.secondary100,
+          fontFamily: theme.fonts.text400,
+        }}>
+        First Commit
+      </Text>
     </View>
   );
 }
